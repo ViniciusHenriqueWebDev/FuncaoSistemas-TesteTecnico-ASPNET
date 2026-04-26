028ca92acc9ac4230783a42e59640b76c1fdfe24
ViniciusHenriqueWebDev <vinicius.henrique2@outlook.com>
2026-04-26 02:37:10 -0300

    Adicionar arquivos de projeto.

---
+++ FI.AtividadeEntrevista/Utils/CpfUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.Utils
+{
+    public static class CpfUtils
+    {
+        public static bool isCpf(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", "").Trim();
+            //Verifica se o CPF tem 11 dígitos
+            if (cpf.Length != 11)
+                return false;
+
+            //Verifica se todos os dígitos são iguais, o que não é um CPF válido
+            if (new string(cpf[0], 11) == cpf)
+                return false;
+
+            //Calcula o primeiro dígito verificador
+            int soma = 0; 
+            for(int i = 0; i < 9; i++)
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            // Calcula o segundo dígito verificador
+            soma = 0; 
+            for(int i = 0; i < 10; i++)
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            //Comparador dos dígitos verificadores calculados com os dígitos finais do CPF
+            string digitosFinais = cpf.Substring(9, 2);
+            string digitosCalculados = digito1.ToString() + digito2.ToString();
+
+            return digitosFinais == digitosCalculados;
+        }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -0,0 +1,92 @@
+﻿
+$(document).ready(function () {
+    if (obj) {
+        $('#formCadastro #Nome').val(obj.Nome);
+        $('#formCadastro #CEP').val(obj.CEP);
+        $('#formCadastro #Email').val(obj.Email);
+        $('#formCadastro #Sobrenome').val(obj.Sobrenome);
+        $('#formCadastro #Nacionalidade').val(obj.Nacionalidade);
+        $('#formCadastro #Estado').val(obj.Estado);
+        $('#formCadastro #Cidade').val(obj.Cidade);
+        $('#formCadastro #Logradouro').val(obj.Logradouro);
+        $('#formCadastro #Telefone').val(obj.Telefone);
+        $('#formCadastro #CPF').val(formatarCPF(obj.CPF));
+    }
+
+    $('#formCadastro #CPF').on('input', function () {
+        let valor = $(this).val();
+        $(this).val(formatarCPF(valor));
+    });
+
+    // Aplica máscara ao perder o foco
+    $('#formCadastro #CPF').on('blur', function () {
+        $(this).val(formatarCPF($(this).val()));
+    });
+
+    // Aplica máscara ao carregar a página
+    $('#formCadastro #CPF').val(formatarCPF($('#formCadastro #CPF').val()));
+    $('#formCadastro').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: urlPost, 
+            method: "POST",
+            data: {
+                "Id": $(this).find("#Id").val(),
+                "Nome": $(this).find("#Nome").val(),
+                "CEP": $(this).find("#CEP").val(),
+                "Email": $(this).find("#Email").val(),
+                "Sobrenome": $(this).find("#Sobrenome").val(),
+                "Nacionalidade": $(this).find("#Nacionalidade").val(),
+                "Estado": $(this).find("#Estado").val(),
+                "Cidade": $(this).find("#Cidade").val(),
+                "Logradouro": $(this).find("#Logradouro").val(),
+                "Telefone": $(this).find("#Telefone").val(),
+                "CPF": $(this).find("#CPF").val().replace(/\D/g, "")
+            },
+            success: function (r) {
+                ModalDialog("Sucesso!", r);
+                $('#' + $('.modal').last().attr('id')).on('hidden.bs.modal', function () {
+                    window.location.href = '/Cliente';
+                });
+            },
+            error: function (r) {
+                if (r.status == 400)
+                    ModalDialog("Ocorreu um erro", r.responseJSON);
+                else if (r.status == 500)
+                    ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
+            }
+        });
+    });
+});
+
+function formatarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "").slice(0, 11);
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    return cpf;
+};
+
+function ModalDialog(titulo, texto) {
+    var random = Math.random().toString().replace('.', '');
+    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
+        '        <div class="modal-dialog">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '                    <p>' + texto + '</p>                                                                           ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+    $('body').append(texto);
+    $('#' + random).modal('show');
+};
